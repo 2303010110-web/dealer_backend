fdfe4d0e6b79120fe63e7a4f608f74335ae1d4b3
20303010110 <2303010110@unper.ac.id>
2026-06-28 14:07:18 +0700

    Menambahkan footer dan skrip sidebar yang ada di dalam folder includes untuk navigasi di halaman admin

---
+++ admin/includes/footer.php
@@ -0,0 +1,15 @@
+  </div><!-- end .content -->
+</div><!-- end .main-wrap -->
+
+<script>
+function openSidebar() {
+  document.getElementById('adminSidebar').classList.add('open');
+  document.getElementById('sideOverlay').classList.add('show');
+}
+function closeSidebar() {
+  document.getElementById('adminSidebar').classList.remove('open');
+  document.getElementById('sideOverlay').classList.remove('show');
+}
+</script>
+</body>
+</html>
